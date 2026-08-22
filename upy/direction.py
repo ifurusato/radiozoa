888d9a2f66d7fcef3b3b5178af2c395f917a831a
ifurusato <ichiro.furusato@gmail.com>
2026-08-22 13:28:31 +1200

    added general motion values

--- upy/direction.py
+++ upy/direction.py
@@ -7,14 +7,15 @@
 #
 # author:   Ichiro Furusato
 # created:  2026-06-27
-# modified: 2026-06-27
+# modified: 2026-08-22
 
 class Direction:
     _instances = {}
 
-    def __init__(self, id, name):
+    def __init__(self, id, name, abbr):
         self._id = id
         self._name = name
+        self._abbr = abbr
         Direction._instances[id] = self
 
     @property
@@ -25,11 +26,25 @@ class Direction:
     def name(self):
         return self._name
 
+    @property
+    def abbrevisation(self):
+        return self._abbr
+
     @classmethod
     def from_id(cls, id):
         return cls._instances.get(id)
 
-OUTBOUND = Direction( 1, "outbound") # downstream, forward
-INBOUND  = Direction(-1, "inbound")  # upstream, reverse
+# used by Relay
+OUTBOUND = Direction( 1, 'outbound',          'outb') # downstream, forward
+INBOUND  = Direction(-1, 'inbound',           'inbd') # upstream, reverse
+
+# general movement directives
+STOPPED           = ( 0, 'stopped',           'stop')
+AHEAD             = ( 2, 'ahead',             'ahed')
+ASTERN            = ( 3, 'astern',            'astn')
+PORT              = ( 4, 'port',              'port')
+STBD              = ( 5, 'starboard',         'stbd')
+CLOCKWISE         = ( 6, 'clockwise',         'clws')
+COUNTER_CLOCKWISE = ( 7, 'counter-clockwise', 'ccwz')
 
 #EOF
